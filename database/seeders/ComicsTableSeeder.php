04b52f44644c15dfc0f559a8dff1069477260e6d
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-16 13:39:06 +0100

    [add] route

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // qui prendo i dati da config dove ho salvato i comics
+        $comics = config('comics');
+        foreach($comics as $comic){
+            $new_comic = new Comic();
+            $new_comi
+        }
+    }
+}
